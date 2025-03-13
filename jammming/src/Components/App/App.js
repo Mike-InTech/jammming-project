0343e7714a8328c35cd527abdc6f6081d01a0b79
Daniel Taylor <DTaylor@nmcc.biz>
2025-03-13 15:13:35 -0500

    Adjusted App.js to match template

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -3,10 +3,21 @@ import SearchBar from '../SearchBar/SearchBar';
 
 function App() {
   return (
-    <div className="App">
-      <p>Jammming!</p>
-      <SearchBar  />
+    <div>
+      <h1>
+        Ja<span className="highlight">mmm</span>ing!
+      </h1>
+      <div className="App">
+        <SearchBar  />
+
+        <div className='App-playlist'>
+          {/* <!-- Add a SearchResults component --> */}
+          {/* <!-- Add a Playlist component --> */}
+          
+        </div>
+      </div>
     </div>
+    
   );
 }
 
